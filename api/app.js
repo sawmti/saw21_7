39e7142dafd13aa2f564f450ee3e44b00d7a98bf
Alfonso Sanchez <asanchez120@gmail.com>
2022-06-02 21:13:33 -0400

    fix api-docs

--- api/app.js
+++ api/app.js
@@ -28,7 +28,10 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: "https://web-exoplanetas.herokuapp.com/",
+      },
+      {
+        url: "https://localhost:4000/",
       },
     ],
   },
